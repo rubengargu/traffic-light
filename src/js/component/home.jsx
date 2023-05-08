2671582b3b0fda7df4b32dd76a2b44e18da93777
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-05-08 19:28:16 +0000

    08/05 ok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [greenColor, greenChange] =useState(true)
+	const [yellowColor, yellowChange] = useState(true)
+	const [redColor, redChange] = useState(true)
+	
+	const greenLight = () => {
+		return greenColor ? <button className="btn btn-success">Verde encendido</button>
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="text-center container ">
+			<p>Hello</p>
+			<div className="col-2 bg-dark text-center">
+				<ul className="p-1">
+				<li className=" {estado.rojo ? 'rojo encendido' : 'rojo apagado'} bg-danger rounded-circle col-6"></li>
+				<li className=" {estado.amarillo ? 'amarillo encendido' : 'amarillo apagado'} bg-warning rounded-circle col-6"></li>
+				<li className=" {estado.verde ? 'verde encendido' : 'verde apagado'} bg-success rounded-circle col-6"></li>
+				</ul>
+			</div>
 		</div>
+		
 	);
 };
-
+const greenLight =() =>{
+	return greenColor ? 
+}
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React,{useState} from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
@@ -10,3 +10,60 @@ import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+const greenDisabled = <button className="btn btn-success">Verde ko</button>
+const greenActive =<button className="btn brn-primary">Verde ok</button>
+
+
+function Semáforo() {
+    const [estado, setEstado] = useState({
+      rojo: false,
+      amarillo: false,
+      verde: false
+    });
+  
+    return (
+      <div>
+        <div className={estado.rojo ? 'rojo encendido' : 'rojo apagado'}></div>
+        <div className={estado.amarillo ? 'amarillo encendido' : 'amarillo apagado'}></div>
+        <div className={estado.verde ? 'verde encendido' : 'verde apagado'}></div>
+      </div>
+    );
+  }
+  const greenLight =() =>{
+    return greenColor ? greenColor :greenChange(true);
+  }
+  
+  javascript
+ 
+  function Semáforo() {
+    const [estado, setEstado] = useState({
+      rojo: false,
+      amarillo: false,
+      verde: false
+    });
+  
+    function encenderRojo() {
+      setEstado({
+        rojo: true,
+        amarillo: false,
+        verde: false
+      });
+    }
+  
+    return (
+      <div>
+        <div className={estado.rojo ? 'rojo encendido' : 'rojo apagado'}></div>
+        <div className={estado.amarillo ? 'amarillo encendido' : 'amarillo apagado'}></div>
+        <div className={estado.verde ? 'verde encendido' : 'verde apagado'}></div>
+        <button onClick={encenderRojo}>Rojo</button>
+      </div>
+    );
+  }
+
+  
+  
+  
+  
+  
+  
