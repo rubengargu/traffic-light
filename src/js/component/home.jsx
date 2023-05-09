44291ffd2963413a94013a032c825eb170b90f84
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-05-09 10:50:11 +0000

    09/05 12:50

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,40 +5,51 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	
+
+  const [rojo, setRojo] = useState(false);
+  const [amarillo, setAmarillo] = useState(false);
+  const [verde, setVerde] = useState(false);
+  console.log(rojo,amarillo,verde);
+
+  const handleSemaforo = (id) =>{
+		switch(id){
+			case "red":
+				setRojo(true);
+				setAmarillo(false);
+				setVerde(false);
+			case "yellow":
+				setRojo(false);
+				setAmarillo(true);
+				setVerde(false);
+
+			case "green":
+				setRojo(false);
+				setAmarillo(false);
+				setVerde(true);
+		}
+  }
+
 	return (
 		
 		<div className="text-center container ">
 			<p>Hello</p>
-			<div className="col-1 bg-dark text-center d-flex">
-				<ul className="p-1">
-				<li className="btn red bg-danger rounded-circle col-6 mb-1"></li>
-				<li className="btn yellow bg-warning rounded-circle col-6 mb-1"></li>
-				<li className="btn green bg-success rounded-circle col-6 mb-1"></li>
+			<div className="col-1 bg-dark text-center ">
+				<ul className="p-1 d-flex flex-column justify-content-center">
+				<li onClick={()=>handleSemaforo(id)} id="red" className="btn bg-danger rounded-circle col-6 mb-1"></li>
+				<li onClick={()=>handleSemaforo(id)} id="yellow" className="btn bg-warning rounded-circle col-6 mb-1"></li>
+				<li onClick={()=>handleSemaforo(id)} id="green" className="btn bg-success rounded-circle col-6 mb-1"></li>
 				</ul>
 			</div>
 		</div>
 		
 	);
 };
-let  semaforo =()=>{
-	const [rojo, setRojo] = useState(true);
-  const [amarillo, setAmarillo] = useState(false);
-  const [verde, setVerde] = useState(false);
 
-}
-function cambiarLuz() {
-    if (rojo) {
-      setRojo(false);
-      setVerde(true);
-    } else if (verde) {
-      setVerde(false);
-      setAmarillo(true);
-    } else {
-      setAmarillo(false);
-      setRojo(true);
-    }
-  }
+
+
+
+
+	
 
 
 export default Home;
