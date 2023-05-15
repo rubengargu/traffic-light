15a8e18841a5948419e827de731b9614a33ff548
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-05-15 19:23:59 +0000

    more beautyful!

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -36,17 +36,18 @@ const Home = () => {
   }
 
 
-.
+
 	return (
 		
-		<div className="text-center container ">
-			<p>Hello</p>
-			<div className="col-1 bg-dark text-center ">
-				<ul className={"p-1 d-flex flex-column justify-content-center"}>
-				<li onClick={() => handleSemaforo('red')} id="red" className={`btn bg-danger rounded-circle col-6 mb-1 ${rojo && "selected"}`} ></li>
-				<li onClick={(e)=>handleSemaforo(e.target.id,)} id="yellow" className={`btn bg-warning rounded-circle col-6 mb-1 ${amarillo && "selected"}`}  ></li>
-				<li onClick={(e)=>handleSemaforo(e.target.id)} id="green" className={`btn bg-success rounded-circle col-6 mb-1, ${verde && "selected"}`} ></li>
-				</ul>
+		<div className="bg-image container ">
+			
+			<div className="base bg-black mx-auto col-1 d-flex justify-content-center">
+			<div className=" d-flex flex-column justify-content:center p-2">
+				<div onClick={() => handleSemaforo('red')} id="red"  className={`btn bg-danger text-center rounded-circle col-1 mb-1 ${rojo && "selected"}`} ></div>
+				<div onClick={(e)=>handleSemaforo(e.target.id,)} id="yellow" className={`btn bg-warning rounded-circle col-1  mb-1 ${amarillo && "selected"}`}></div>
+				<div onClick={(e)=>handleSemaforo(e.target.id)} id="green" className={`btn bg-success rounded-circle col-1  mb-1, ${verde && "selected"}`}></div>
+				</div>
+			
 			</div>
 		</div>
 		
