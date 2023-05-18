be2bb1a6c850dbc837722a47c6308d5cd392df30
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2023-05-18 17:17:42 +0000

    I love the lights

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "../../styles/index.css";
 
 
-
 //create your first component
 const Home = () => {
 
@@ -34,22 +33,18 @@ const Home = () => {
 		}
 	
   }
-
-
-
 	return (
 		
-		<div className="bg-image container ">
-			
-			<div className="base bg-black mx-auto col-1 d-flex justify-content-center">
-			<div className=" d-flex flex-column justify-content:center p-2">
-				<div onClick={() => handleSemaforo('red')} id="red"  className={`btn bg-danger text-center rounded-circle col-1 mb-1 ${rojo && "selected"}`} ></div>
-				<div onClick={(e)=>handleSemaforo(e.target.id,)} id="yellow" className={`btn bg-warning rounded-circle col-1  mb-1 ${amarillo && "selected"}`}></div>
-				<div onClick={(e)=>handleSemaforo(e.target.id)} id="green" className={`btn bg-success rounded-circle col-1  mb-1, ${verde && "selected"}`}></div>
-				</div>
-			
-			</div>
+		<div className="text-center container">
+		<div className="base  mx-auto col-3  traffic">
+		  <ul className="p-1 col-6 d-flex flex-column justify-content-center align-items-center text-center">
+			<li onClick={() => handleSemaforo('red')} id="red" className={`btn bg-danger rounded-circle h-100 col-6 mb-1 mt-4 ${rojo && "selected-red"} `}></li>
+			<li onClick={(e)=>handleSemaforo(e.target.id)} id="yellow" className={`btn bg-warning rounded-circle col-6 h-100 mt-1 mb-1 ${amarillo && "selected-yellow"} `}></li>
+			<li onClick={(e)=>handleSemaforo(e.target.id)} id="green" className={`btn bg-success rounded-circle col-6 h-100 mt-1 mb-4 ${verde && "selected-green"} `}></li>
+		  </ul>
 		</div>
+	  </div>
+	  
 		
 	);
 };
